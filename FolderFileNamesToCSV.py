4021b6d2f37b3db029d046dc6c9a24caf199440a
Chris Ma <87343474+chow2n@users.noreply.github.com>
2022-05-03 14:59:25 -0700

    Update FolderFileNamesToCSV.py

--- FolderFileNamesToCSV.py
+++ FolderFileNamesToCSV.py
@@ -1,12 +1,14 @@
 import os
 import pandas as pd
 
+path = 'your_own/filepath/'
+
 # Create empty list to store data for Pandas DataFrame
 data = []
 
 # Use os.listdir to iterate through the folder names and file names in file path
-for folder in sorted(os.listdir(filepath)):
-    for file in sorted(os.listdir(filepath + folder)):
+for folder in sorted(os.listdir(path)):
+    for file in sorted(os.listdir(path + folder)):
     # Use .endswith to filter only ".pdf" and ".tiff" file extension names
         if file.endswith((".pdf", ".tiff")):
             data.append((folder, file))
